02c27904effe3cc74b8cf0c1fe505ecd90c2aa0c
lfrancisco <franciscoaglima@gmail.com>
2021-05-23 21:04:29 -0300

    2o - Etapa - Completo Aula Projeto Form Users

--- lib/formuserpage.dart
+++ lib/formuserpage.dart
@@ -4,57 +4,70 @@ class FormUserPage extends StatefulWidget {
   @override
   _FormUserPageState createState() => _FormUserPageState();
 }
-
 class _FormUserPageState extends State<FormUserPage> {
 
-  // final _formKey = GlobalKey<FormState>();
-
+  final _formKey = GlobalKey<FormState>();
   String name = "";
-
+  String sobname = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//    appBar: AppBar(title: Text("Form Login"),),
+    appBar: AppBar(title: Text("Form Login"),),
+        body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (text) {
+                   if(text == null || text.isEmpty) {
+                  return "Texto é Obrigatório";
+                   }
+                    return null;
+                  },
+             decoration: InputDecoration(
+               hintText: "Nome",
+             ),
+             onChanged: (text) {
+                name = text;
+             },
+            ),
 
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                     barrierDismissible: true,
-                     context: context,
-                     builder: (context) {
-                       return AlertDialog(
-                           title: Text("Titulo de Alerta"),
-                         content: Text("Flutter é Maravilhoso !"),
-                         actions: [
-                           TextButton(
-                             onPressed: () {
-                                Navigator.pop(context);
-                               }, 
-                            child: Text("Sim"),     
-                           ),
-                           ElevatedButton(
-                             onPressed:() {
-                             Navigator.pop(context);
-                           },
-                           child: Text("Claro"),
-                           ),
-                         ],
-                       ); 
-                  });
-                 },
-                 child: Text("Alerta"),
-                ),
-                
-              ),
-            ],
+            TextFormField(
+              // ignore: missing_return
+              validator: (text) {
+                if(text == null || text.isEmpty) {
+                  return "SobreNome é Obrigatório";
+                }
+                return null;                
+              },
+             decoration: InputDecoration(
+               hintText: "SobreNome",
+             ),
+             onChanged: (text) {
+                sobname = text;
+             },              
+            ),
+
+                ElevatedButton( 
+                  onPressed: () { 
+                    final isValid = _formKey.currentState!.validate();
+
+                    if (isValid) {
+                      showDialog(
+                        context: context, 
+                        builder: (context) {
+                          return AlertDialog( 
+                            title: Text("$name $sobname"),
+                          );
+                        });
+                    }
+                  },
+                  child: Text("Validar"),
+                )
+             ],
+            ),
           ),
-        ),
      );
   }
 }
